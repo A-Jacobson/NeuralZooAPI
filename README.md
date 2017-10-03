34f73345e16cc6b6d94d08c131bcc9115010e447
austin <jacobsonaustinj@gmail.com>
2017-10-03 06:34:18 -0700

    update readme

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # ZooAPI
-Machine Learning API for DL models I have trained
+Machine Learning API for DL models
+
+
+## Milestones
+- [x] Example endpoint with torchvision models
+- [ ] Depth in the Wild
+- [ ] Super Resolution
+- [ ] CoNLL Entity Detection
+- [ ] Amazon Reviews Sentiment Analysis
+- [ ] Word2Vec
+- [ ] Dockerize
+- [ ] Deploy to AWS
 
 ## Endpoints
 ![title](api_spec.png)
--- apis/imagenet.py
+++ apis/imagenet.py
@@ -5,7 +5,6 @@ from werkzeug.datastructures import FileStorage
 
 from core.models import resnet
 
-
 api = Namespace('imagenet', description='Models Trained on ImageNet')
 
 upload_parser = api.parser()
@@ -18,9 +17,8 @@ upload_parser.add_argument('file', location='files',
 class ResNet18(Resource):
     def post(self):
         args = upload_parser.parse_args()
-        uploaded_file = args['file'] # This is FileStorage instance
+        uploaded_file = args['file']  # This is FileStorage instance
         image = Image.open(uploaded_file.stream)
         resnet18 = resnet.ResNet18()
         prediction = resnet18.predict(image)
         return prediction
-
